225e81c5cea8d45292591a656d396867b320bd08
weibo <18872231417@163.com>
2022-05-31 14:45:42 +0800

    Initial commit

---
+++ src/main/java/com/wwb/errorbase/ErrorbaseApplication.java
@@ -0,0 +1,13 @@
+package com.wwb.errorbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorbaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wwb/errorbase/ErrorbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwb.errorbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
